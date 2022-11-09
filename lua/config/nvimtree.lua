4eb859082548feebdffdbbd920de665f1d7a8082
Joe Finn <joseph.finn@imc.com>
2022-11-09 16:11:21 -0600

    Change default size for nvim tree

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -1,4 +1,13 @@
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup {
+    view = {
+        width = 45,
+    },
+    actions = {
+        open_file = {
+            resize_window = false
+        }
+    }
+}
