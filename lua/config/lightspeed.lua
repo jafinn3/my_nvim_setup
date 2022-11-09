ba7040ae99b413022d43c80ae0f845acf829eaa0
Joe Finn <joseph.finn@imc.com>
2022-11-09 16:11:36 -0600

    Lightspeed: Fix repeat command, get rid of safe labels

--- lua/config/lightspeed.lua
+++ lua/config/lightspeed.lua
@@ -1,7 +1,7 @@
 require'lightspeed'.setup {
     ignore_case = true,
     exit_after_idle_msecs = { labeled = 750, unlabeled = 1000 },
-    safe_labels = {'a', 'r', 's', 't', 'n', 'e', 'i', 'o', 'd', 'h', 'p', 'l'},
-    -- arstneiodhplfuvkcmbyw;xz[/.ARSTNEIO
-    labels = {'a', 'r', 's', 't', 'n', 'e', 'i', 'o', 'd', 'h', 'p', 'l', 'f', 'u', 'v', 'k', 'c', 'm', 'b', 'y', 'w', ';', 'x', 'z', '[', '/', '.', ',', 'A', 'R', 'S', 'T', 'N', 'E', 'I', 'O'}
+    safe_labels = {},
+    -- arsneiodhplfuvkcmbyw;xz[/.ARSTNEIO
+    labels = {'a', 'r', 's', 'n', 'e', 'i', 'o', 'd', 'h', 'p', 'l', 'f', 'u', 'v', 'k', 'c', 'm', 'b', 'y', 'w', ';', 'x', 'z', '[', '/', '.', ',', 'A', 'R', 'S', 'T', 'N', 'E', 'I', 'O'}
 }
