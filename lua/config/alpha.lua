51f393cc9ffd8166aed8a34b56659659ce1e22d9
Joe Finn <joseph.finn@imc.com>
2022-11-09 16:12:21 -0600

    Add some commands to alpha dashboard

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -17,7 +17,12 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
     dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
     dashboard.button( "f", "  > Find file", ":Files<CR>"),
-    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "t", "  > Find text", ":Rg<CR>"),
+    dashboard.button( "gd", "  > Git commit history", ":DiffviewFileHistory<CR>"),
+    dashboard.button( "gdo", "  > Git diff", ":DiffviewOpen -uno<CR>"),
+    dashboard.button( "i", "襁 > Install Plugins"   , ":PlugInstall<CR>"),
+    dashboard.button( "u", "  > Update plugins"   , ":PlugUpdate<CR>"),
+    dashboard.button( "c", "  > Clean plugins"   , ":PlugClean<CR>"),
     dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | pwd<CR>"),
     dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
 }
