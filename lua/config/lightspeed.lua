010863cccd10fbdd1c882428db3bff25e4793ee2
Joe Finn <joseph.finn@imc.com>
2023-10-12 18:04:15 -0500

    Updating lightspeed mappings

--- lua/config/lightspeed.lua
+++ lua/config/lightspeed.lua
@@ -2,6 +2,6 @@ require'lightspeed'.setup {
     ignore_case = true,
     exit_after_idle_msecs = { labeled = 750, unlabeled = 1000 },
     safe_labels = {},
-    -- arsneiodhplfuvkcmbyw;xz[/.ARSTNEIO
-    labels = {'a', 'r', 's', 'n', 'e', 'i', 'o', 'd', 'h', 'p', 'l', 'f', 'u', 'v', 'k', 'c', 'm', 'b', 'y', 'w', ';', 'x', 'z', '[', '/', '.', ',', 'A', 'R', 'S', 'T', 'N', 'E', 'I', 'O'}
+    -- arsneiodhplfuvkcmbywxz[/.ARSTNEIO
+    labels = {'a', 'r', 's', 'n', 'e', 'i', 'o', 'd', 'h', 'p', 'l', 'f', 'u', 'v', 'k', 'c', 'm', 'b', 'y', 'w', 'x', 'z', '[', '/', '.', ',', 'A', 'R', 'S', 'T', 'N', 'E', 'I', 'O'}
 }
