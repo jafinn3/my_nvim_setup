db16a257afe6d6deda7523f6d6a0001ce67bbd07
Joe Finn <joseph.finn@imc.com>
2023-10-12 18:04:25 -0500

    More dap config

--- lua/config/nvim-dap.lua
+++ lua/config/nvim-dap.lua
@@ -14,7 +14,7 @@ local vscode_cpptools = {
     miDebuggerServerAddress = 'localhost:5678',
     miDebuggerPath = '/opt/imc/gdb/bin/gdb',
     cwd = '${workspaceFolder}',
-    program=""
+    program="/home/user/.cache/bazel/_bazel_jfinn/2f06d073fd3230d438afb64afae646ca/execroot/roadrunner/bazel-out/k8-dbg--gccdebug/bin/hayabusa/nasdaq-options/test/component/fpgavaluationtriggeredengine/cme-multi-threshold-masscancel/_tsm-tte-component-test"
 
 }
 
@@ -41,3 +41,8 @@ end
 
 dapui.setup()
 
+vim.api.nvim_set_keymap("n", "<F4>", ":lua require('dapui').toggle()<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<F5>", ":lua require('dap').toggle_breakpoint()<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<F6>", ":lua require('dap').step_over()<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<F7>", ":lua require('dap').step_out()<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<F8>", ":lua require('dap').continue()<CR>", {noremap = true})
